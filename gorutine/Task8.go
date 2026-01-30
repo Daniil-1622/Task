6e44c41b42c648c8de368bb6fa23a8486a981eeb
Daniil-1622 <dotagum687@gmail.com>
2026-01-30 17:07:06 +0300

    Gorutine

--- gorutine/Task8.go
+++ gorutine/Task8.go
@@ -7,15 +7,22 @@ import (
 )
 
 func main() {
+	//Создаем WaitGroup
 	wg := sync.WaitGroup{}
 
+	// Записываем сколько горутин будет всего, в нашем случае 5
 	wg.Add(5)
 	go func() {
+		// Проходимся от 1 до 5
 		for i := 1; i < 6; i++ {
+			// Для каждой итерации wg.Done()
 			defer wg.Done()
+			// Выводим число
 			fmt.Println(i)
+			// Делаем паузу 200 мс
 			time.Sleep(200 * time.Millisecond)
 		}
 	}()
+	// Расположения нашей горутины в main()
 	wg.Wait()
 }
